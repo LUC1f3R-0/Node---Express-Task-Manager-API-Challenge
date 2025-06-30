75a5c23eb26783982a5abec7b2f57919eb094c1f
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-06-30 13:51:05 +0530

    updated app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import express from 'express';
+import mainRoute from './routes/mainRoute.js';
+import cors from 'cors';
+import 'dotenv/config'
 
 
 const app = express();
 
+app.use(express.json())
+app.use(cors({ origin: process.env.CLIENT_ORIGIN, credentials: true }))
+
+app.use("/api/protected/", mainRoute)
 
 
 export default app;
