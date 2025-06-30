0a28c06c80405a7ed952e5a1951cde4f3af672d1
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-06-30 13:52:35 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import app from "./src/app.js";
+import sequelize from "./src/config/database.connection.config.js";
 
 
 
+(async () => {
+    try {
+        await sequelize.authenticate();
+        console.log("---- DATABASE CONNECTION SUCCESSFUL ----")
 
-
-
-app.listen(1000, () => console.log("server runs on http://localhost:1000"))
+        app.listen(2000, () => console.log("server runs on http://localhost:2000"))
+    } catch (err) {
+        console.log("---- DATABASE CONNECTION FAIL ----")
+        console.error("Message:", err.message);
+        console.error("Code:", err.original?.code);
+        process.exit(1);
+    }
+})();
